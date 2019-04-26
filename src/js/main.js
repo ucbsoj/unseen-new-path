a3a73949f8e6996bf07215aa1b9e52c3f07c8b51
Lucio Villa <lucio310@gmail.com>
2019-04-25 21:25:22 -0700

    working on topper

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,7 @@
 import $ from "jquery"; // importing jQuery, you can delete if not needed
 require("./lib/social"); // Twitter intent JS
+import Flickity from "flickity";
 
 
-
-var imgReveal = $('#top-quote').offset().top;
-
-
-
-$(document).scroll(function() {
-
-  var scroll = $(document).scrollTop();
-
-
-  if (scroll > 300) {
-
-  	$(".bg-img").css({"-webkit-filter": "blur(5px)","filter": "blur(5px)" })  
-
-  } else {
-
-  	$(".bg-img").css({"-webkit-filter": "blur(10px)","filter": "blur(10px)" })  
-  
-  }
-
-  if (scroll > imgReveal){
-
-  	$(".bg-img").css({"-webkit-filter": "blur(0px)","filter": "blur(0px)" })  
-  
-  } 
-
-
-});
+var flky = new Flickity( '.carousel', {});
 
