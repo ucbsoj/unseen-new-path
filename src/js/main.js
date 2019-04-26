c88e21943e8c19a2d845abbb23669bebef401d9b
Lucio Villa <lucio310@gmail.com>
2019-04-25 22:50:08 -0700

    updated flickity bug

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,8 @@ require("./lib/social"); // Twitter intent JS
 import Flickity from "flickity";
 
 
-var flky = new Flickity( '.carousel', {});
+var flky = new Flickity( '#graphics-one', {});
+var flky = new Flickity( '#graphics-two', {});
 
  var iframe = document.querySelector('iframe');
     var player = new Vimeo.Player(iframe);
